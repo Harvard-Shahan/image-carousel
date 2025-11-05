87abb06814aba97579a1edd5fe4241e37c15cd24
doomslayee <rs.subhaarama@gmail.com>
2025-11-05 23:44:04 +0530

    Added jump buttons to jump to an image

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 const prev=document.querySelector("#prev");
 const next=document.querySelector('#next');
+function addButton(){
+const btnContainer=document.querySelector(".btnContainer");
+const imgs=document.querySelectorAll(".img");
+for(let i=0;i<imgs.length;i++){
+    let btn=document.createElement("button");
+    btn.textContent=i;
+    btn.addEventListener("click",()=>{
+        let j=0;
+    for(;j<imgs.length;j++)
+        if(imgs[j].classList.contains('hidden'))
+            continue;
+        else
+        break;
+    imgs[j].classList.toggle('hidden');
+    imgs[i].classList.toggle('hidden');
+    if(i==0)
+    {
+        next.disabled=false;
+        prev.disabled=true;
+    }
+    if(i==imgs.length-1){
+        next.disabled=true;
+        prev.disabled=false;
+    }
+    })
+    btnContainer.appendChild(btn);
+}
+}
 prev.addEventListener("click",()=>{
+    
     next.disabled=false;
     const imgs=document.querySelectorAll(".img");
     let i=0;
@@ -28,3 +57,4 @@ next.addEventListener("click",()=>{
 imgs[i].classList.toggle('hidden');
      imgs[i+1].classList.toggle('hidden');   
 })
+addButton();
